80345db27168f28d34ccad3c5951ab53caff95ce
Daniel Pencak <danielpencak@gmail.com>
2017-08-05 18:29:24 -0500

    Add headshot

--- src/About/About.js
+++ src/About/About.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import Picture from './Assets/picture.JPG';
+import './About.css';
 
 export default class About extends Component {
   render() {
     return (
       <div className="About">
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} md={4} className="pic">
+              <img className="headshot" src={ Picture } alt="Headshot"/>
+            </Col>
+            <Col xs={6} md={8} className="summary"></Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
