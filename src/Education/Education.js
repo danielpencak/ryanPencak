9bb8938d18fab613c7a58d808fdc910f32e19198
Daniel Pencak <danielpencak@gmail.com>
2017-08-18 15:56:59 -0500

    Add style adjustments to education

--- src/Education/Education.js
+++ src/Education/Education.js
@@ -10,7 +10,7 @@ export default class Education extends Component {
           <Row>
             <Col>
               <h1>Education</h1>
-              <h3><b>Bucknell University, College of Engineering</b> | Lewisburg, PA <span>May 2018</span></h3>
+              <h3 className="school"><b>Bucknell University, College of Engineering</b> <b className="bar">|</b> Lewisburg, PA <span>May 2018</span></h3>
               <h4>B.S. Computer Engineering</h4>
               <p>GPA: 3.5 | Dean's List</p>
             </Col>
